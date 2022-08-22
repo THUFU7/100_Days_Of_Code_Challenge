d1d52b50b018f2bc25c927747a8554211d7344b4
ROBERT MUNGAI <92425223+THUFU7@users.noreply.github.com>
2022-08-22 15:47:21 +0300

    Add files via upload

---
+++ Day_20_of_100/src/main/kotlin/Main.kt
@@ -0,0 +1,29 @@
+fun main(args: Array<String>) {
+    //Kotlin recap
+   var myVariable = true
+    println("The value of the variable is: $myVariable")
+    myVariable = false
+    println("The value now is: $myVariable")
+    //homework
+    var myAge = 23
+    var favNumber = 7
+    println("My favourite number is $favNumber and I am $myAge years old")
+    //Arithmetic operator
+    var x = 7
+    var y = 10
+    println("The result is: ${x * y}")
+    //homework
+    var m = 3
+    println("m: $m")
+    var n = 4
+    println("m + n: ${m + n}")
+    //homework
+    val PI: Double = 3.141592653589793
+    var radius = 5.5
+    val formula = 0.75 * PI * (radius * radius *radius)
+    println("The volume of a sphere of radius 5.5 is: $formula")
+
+
+
+
+}
